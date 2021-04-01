f3129688bc580f6e767239910947e9d0fe71fbd9
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-04-01 19:18:14 +0900

    feat: Modify code in solution2 in 35.extract intersection elements problem

--- basic/section5/35_extract_intersection_elements.js
+++ basic/section5/35_extract_intersection_elements.js
@@ -32,7 +32,7 @@ function solution(arr1, arr2) {
     arr2.sort();
     let p1 = p2 = 0;
     while (p1 < arr1.length && p2 < arr2.length) {
-        if (arr1[p1] == arr2[p2]) {
+        if (arr1[p1] === arr2[p2]) {
             answer.push(arr1[p1++]);
             p2++;
         } else if (arr1[p1] < arr2[p2]) p1++;
